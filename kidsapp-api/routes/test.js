f173edfdcfbaedcef214f92d2491f64ef19585a6
Steve <stevenlyall@gmail.com>
2016-01-23 20:33:10 -0800

    fix service script

--- kidsapp-api/routes/test.js
+++ kidsapp-api/routes/test.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET test listing. */
-router.get('/', function(req, res, next) {
+router.get('/test', function(req, res, next) {
   res.send('respond with a resource');
 });
 
